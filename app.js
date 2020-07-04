e4670173ecdc7920c2ec4d8b37c6d449b59af456
germanbobadilla <germanbobadilla@gmail.com>
2020-07-04 09:50:09 -0400

    New changes

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-	res.sendFile('./views/index.html', { root: __dirname });
+	res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
-	res.sendFile('./views/about.html', { root: __dirname });
+	res.render('about', { title: 'About' });
 });
 
+app.get('/blogs/create', (req, res) => {
+	res.render('create', { title: 'Create' });
+});
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-})
-
-app.use((res, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
-})
+app.use((req, res) => {
+	res.status(404).render('404', { title: '404' });
+});
